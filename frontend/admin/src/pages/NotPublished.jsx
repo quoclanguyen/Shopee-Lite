0f4209a5ee624b8227e89d2b15a281872706312d
2506lanlan <91785700+2506lanlan@users.noreply.github.com>
2023-12-01 18:49:11 +0700

    Edit frontend/admin

--- frontend/admin/src/pages/NotPublished.jsx
+++ frontend/admin/src/pages/NotPublished.jsx
@@ -1,46 +1,46 @@
 import { Modal, message } from 'antd';
 import React, { useState } from 'react';
-import { BsTrash3Fill } from "react-icons/bs";
+import { BsArrow90DegLeft } from "react-icons/bs";
 import { FaPlus } from "react-icons/fa6";
 import { MdModeEdit } from "react-icons/md";
 import ProductForm from '../components/ProductForm';
 import Table from '../components/Table';
 import AdminLayout from '../layout';
-import { deleteProduct, useFindAllProduct } from "../services/product";
+import { publishProduct, useFindAllDeletedProducts } from "../services/product";
 import { displayCurrencyVND } from '../utils';
 import Loading from '../components/Loading';
 import { FormProvider, useForm, useFormContext } from 'react-hook-form';
 
-const Product = () => {
+const NotPublished = () => {
   const [messageApi, contextHolder] = message.useMessage();
   const [modalOpen, setModalOpen] = useState(false);
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
   const [isEditForm, setIsEditForm] = useState(false);
   const [product, setProduct] = useState(null)
-  const { data: products, isLoading } = useFindAllProduct()
+  const { data: products, isLoading } = useFindAllDeletedProducts()
   const handleCancel = () => {
     setModalOpen(false);
     setIsEditForm(false)
   };
-  const handleDeletePopupCancel = () => {
+  const handlePublishPopupCancel = () => {
     setDeleteModalOpen(false);
     setIsEditForm(false)
   };
-  const handleDeleteProduct = async (product) => {
-    handleDeletePopupCancel();
+  const handlePublishProduct = async (product) => {
+    handlePublishPopupCancel();
     const key = 'delete';
     messageApi.open({
       key,
       type: 'loading',
-      content: 'Đang xóa...',
+      content: 'Đang xử lý mở bản...',
     });
-    const response = await deleteProduct(product._id, product);
+    const response = await publishProduct(product._id, product);
     console.log({ response })
     setTimeout(() => {
       messageApi.open({
         key,
         type: 'success',
-        content: 'Đã xóa ' + product.product_name + ' thành công!',
+        content: 'Đã mở bán ' + product.product_name + ' thành công!',
         duration: 3,
       });
     }, 1500);
@@ -83,23 +83,13 @@ const Product = () => {
       key: 'action',
       width: 200,
       render: (_, record) => (
-        <div className="items-center">
-          <div className='py-1'>
-            <MdModeEdit onClick={() => {
-              setModalOpen(true);
-              setIsEditForm(true);
-              setProduct(record)
-            }} className='cursor-pointer hover:text-red-500 hover:scale-105 inline-block' />
-            <p className='inline-block px-2'>Chỉnh sửa</p>
-          </div>
-          <div>
-            <BsTrash3Fill onClick={() => {
-              setDeleteModalOpen(true);
-              setIsEditForm(true);
-              setProduct(record)
-            }} className='cursor-pointer hover:text-red-500 hover:scale-105 inline-block' />
-            <p className='inline-block px-2'>Xóa sản phẩm</p>
-          </div>
+        <div className='py-1'>
+          <BsArrow90DegLeft onClick={() => {
+            setDeleteModalOpen(true);
+            setIsEditForm(true);
+            setProduct(record)
+          }} className='cursor-pointer hover:text-red-500 hover:scale-105 inline-block' />
+          <p className='inline-block px-2'>Mở bán sản phẩm</p>
         </div>
       ),
     },
@@ -110,33 +100,21 @@ const Product = () => {
     <AdminLayout>
       {contextHolder}
       <div className='m-8 h-full'>
-        <h1 className="font-bold text-3xl p-4">Danh mục sản phẩm</h1>
-        <button className="bg-gray-800 rounded-md px-4 py-2 text-white mb-4 flex items-center gap-x-2" onClick={() => { setModalOpen(true); setIsEditForm(false) }}>
-          <FaPlus />
-          <span>Tạo sản phẩm mới</span></button>
+        <h1 className="font-bold text-3xl p-4">Sản phẩm chưa mở bán</h1>
         {isLoading ? <Loading /> : <Table data={products.metadata} columns={columns} />}
       </div>
-      <Modal title={<span>Xóa <strong>{isEditForm && product.product_name}</strong> hả?</span>} open={deleteModalOpen} onCancel={handleDeletePopupCancel} className='w-[10vw] h-[10vw]'
+      <Modal title={<span>Mở bán <strong>{isEditForm && product.product_name}</strong> hả?</span>} open={deleteModalOpen} onCancel={handlePublishPopupCancel} className='w-[10vw] h-[10vw]'
         footer={[
           <button
-            onClick={handleDeletePopupCancel}
+            onClick={handlePublishPopupCancel}
             className="px-4 py-1 rounded-md bg-white border text-red-500 border-red-500 mr-2">Hủy</button>,
           <button
-            onClick={() => handleDeleteProduct(product)}
-            className="px-4 py-1 rounded-md bg-red-500 text-white">OK</button>
+            onClick={() => handlePublishProduct(product)}
+            className="px-4 py-1 rounded-md bg-green-500 text-white">OK</button>
         ]}
       ></Modal>
-      <Modal
-        title={<h1 className="text-3xl font-bold">{isEditForm ? product.product_name : "Thêm sản phẩm"}</h1>}
-        open={modalOpen}
-        footer={null}
-        onCancel={handleCancel}
-        className='min-w-[50vw]'
-      >
-        <ProductForm record={product} isEditForm={isEditForm} methods={{ setModalOpen }} />
-      </Modal>
     </AdminLayout>
   )
 }
 
-export default Product
+export default NotPublished
--- frontend/admin/src/services/product/index.js
+++ frontend/admin/src/services/product/index.js
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import axiosClient from "../../config/axiosClient";
 import { ProductEndpoint } from "./endpoint";
 
@@ -13,34 +13,59 @@ const findAllProducts = async () => {
       return [];
     }
   };
-export const useFindAllProduct =  () => {
-  return useQuery({ queryKey: ['findAllProducts'], queryFn: findAllProducts, staleTime: 1000, refetchInterval: 10000})
-}
-export const createNewProduct = async (payload) => {
+const findAllDeletedProducts = async () => {
   try {
-    const productResponse = await axiosClient.request(ProductEndpoint.createNewProduct(payload));
-    return productResponse;
+    const productResponse = await axiosClient.request(ProductEndpoint.findAllDeleted);
+    return productResponse.data;
+    
   } catch (err) {
     console.log("Error fetching")
     console.log(err)
     return [];
   }
 };
+export const useFindAllProduct =  () => {
+  return useQuery({ queryKey: ['findAllProducts'], queryFn: findAllProducts, staleTime: 1000, refetchInterval: 10000})
+}
+export const useFindAllDeletedProducts = () => {
+  return useQuery({ queryKey: ['findAllDeletedProducts'], queryFn: findAllDeletedProducts, staleTime: 1000, refetchInterval: 10000})
+}
+export const createNewProduct = async (payload) => {
+  try {
+    const productResponse = await axiosClient.request(ProductEndpoint.createNewProduct(payload));
+    return productResponse;
+  } catch (err) {
+    console.log("Error fetching");
+    console.log(err);
+    return [];
+  }
+};
 export const updateProduct = async (id, payload) => {
-  try{
+  try {
     const productResponse = await axiosClient.request(ProductEndpoint.updateProduct(id, payload));
     return productResponse;
-  }catch(error){
-    console.log(err)
+  } catch (error) {
+    console.log(error);
     return [];
   }
-}
+};
 export const deleteProduct = async (id, payload) => {
+  try {
+    const productResponse = await axiosClient.request(
+      ProductEndpoint.deleteProduct(id, { ...payload, isPublished: false })
+    );
+    return productResponse;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+export const publishProduct = async (id, payload) => {
   try{
-    const productResponse = await axiosClient.request(ProductEndpoint.deleteProduct(id, {...payload, isPublished: false}));
+    const productResponse = await axiosClient.request(ProductEndpoint.publishProduct(id, {...payload, isPublished: true}));
     return productResponse;
   }catch(error){
     console.log(err)
     return [];
   }
-}
+};
