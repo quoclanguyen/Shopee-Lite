14f0b73100bac6c04e4c2af93c0d3b5a52f32106
2506lanlan <91785700+2506lanlan@users.noreply.github.com>
2023-12-12 01:15:39 +0700

    Edit somthing in Footer

--- frontend/client/src/components/Footer.tsx
+++ frontend/client/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="flex justify-center items-center py-4 px-8 bg-gray-800 text-white mt-4">
-      <p>&copy; 2023 Nguyen Quoc Lan. All rights reserved.</p>
+      <p>&copy; 2023 Shop DEV. All rights reserved.</p>
     </div>
   );
 }
